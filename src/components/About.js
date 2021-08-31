7b8fcbf1fb6a5d326d4c2f9d680c47838bc97103
Lauri Järvinen <laurijarvinen@hotmail.com>
2021-08-31 16:37:07 +0300

    about-page started

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,25 @@
 import React from "react";
 
+
 const About = () => {
     return (
-        <div className="about">
-            <h2>About me</h2>
-        </div>
+        <div className="about-wrapper">
+       
+
+  
+
+
+
+
+
+               
+            
+        
+  
+  </div>
+        
+
+        
     );
 }
 
