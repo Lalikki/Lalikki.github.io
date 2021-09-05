03ec1163a790b353c996a8676494ae23a3c2d938
Lalikki <laurijarvinen@hotmail.com>
2021-09-05 14:10:52 +0300

    experience page layout done

--- src/components/About.js
+++ src/components/About.js
@@ -4,21 +4,21 @@ import React from "react";
 const About = () => {
     return (
         <div className="about-wrapper">
-       
-       <div className="container mt-5">
-  <h1>About me</h1>
-  
-  <div className="row">
-    <div className="col-sm-4" >Kuva tähän</div>
-    <div className="col-sm-4" >Info tähän</div>
-    <div className="col-sm-4" >Lisää infoo tähän</div>
-  </div>
-            
-</div>      
-  </div>
-        
 
-        
+            <div className="container">
+                <h1>About me</h1>
+
+                <div className="row">
+                    <div className="col-sm-4" >Kuva tähän</div>
+                    <div className="col-sm-4" >Info tähän</div>
+                    <div className="col-sm-4" >Lisää infoo tähän</div>
+                </div>
+
+            </div>
+        </div>
+
+
+
     );
 }
 
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,8 +2,56 @@ import React from 'react';
 
 const Experience = () => {
     return (
-        <div>
-            <h2>My experience</h2>
+        <div className="experience-wrapper">
+
+            <div className="container">
+                <h1>Experience</h1>
+
+                <h4>Job Experience</h4>
+                <div className="row">
+                    <div className="col" >
+                        <p className="content-paragraph">Lorem Ipsum on yksinkertaisesti testausteksti, jota tulostus- ja ladontateollisuudet käyttävät. Lorem Ipsum on ollut teollisuuden normaali
+                            testausteksti jo 1500-luvulta asti, jolloin tuntematon tulostaja otti kaljuunan ja sekoitti sen tehdäkseen esimerkkikirjan.
+                            Se ei ole selvinnyt vain viittä vuosisataa, mutta myös loikan elektroniseen kirjoitukseen, jääden suurinpiirtein muuntamattomana.
+                            Se tuli kuuluisuuteen 1960-luvulla kun Letraset-paperiarkit, joissa oli Lorem Ipsum pätkiä, julkaistiin ja
+                            vielä myöhemmin tietokoneen julkistusohjelmissa, kuten Aldus PageMaker joissa oli versioita Lorem Ipsumista.</p>
+                    </div>
+                    <div className="col" >
+                        <p className="content-paragraph">Lorem Ipsum on yksinkertaisesti testausteksti, jota tulostus- ja ladontateollisuudet käyttävät. Lorem Ipsum on ollut teollisuuden normaali
+                            testausteksti jo 1500-luvulta asti, jolloin tuntematon tulostaja otti kaljuunan ja sekoitti sen tehdäkseen esimerkkikirjan.
+                            Se ei ole selvinnyt vain viittä vuosisataa, mutta myös loikan elektroniseen kirjoitukseen, jääden suurinpiirtein muuntamattomana.
+                            Se tuli kuuluisuuteen 1960-luvulla kun Letraset-paperiarkit, joissa oli Lorem Ipsum pätkiä, julkaistiin ja
+                            vielä myöhemmin tietokoneen julkistusohjelmissa, kuten Aldus PageMaker joissa oli versioita Lorem Ipsumista.</p>
+                    </div>
+
+                </div>
+
+                <h4>Other</h4>
+                <div className="row">
+                    <div className="col">
+                        <p className="content-paragraph">Lorem Ipsum on yksinkertaisesti testausteksti, jota tulostus- ja ladontateollisuudet käyttävät. Lorem Ipsum on ollut teollisuuden normaali
+                            testausteksti jo 1500-luvulta asti, jolloin tuntematon tulostaja otti kaljuunan ja sekoitti sen tehdäkseen esimerkkikirjan.
+                            Se ei ole selvinnyt vain viittä vuosisataa, mutta myös loikan elektroniseen kirjoitukseen, jääden suurinpiirtein muuntamattomana.
+                            Se tuli kuuluisuuteen 1960-luvulla kun Letraset-paperiarkit, joissa oli Lorem Ipsum pätkiä, julkaistiin ja
+                            vielä myöhemmin tietokoneen julkistusohjelmissa, kuten Aldus PageMaker joissa oli versioita Lorem Ipsumista.</p>
+                    </div>
+                    <div className="col">
+                        <p className="content-paragraph">Lorem Ipsum on yksinkertaisesti testausteksti, jota tulostus- ja ladontateollisuudet käyttävät. Lorem Ipsum on ollut teollisuuden normaali
+                            testausteksti jo 1500-luvulta asti, jolloin tuntematon tulostaja otti kaljuunan ja sekoitti sen tehdäkseen esimerkkikirjan.
+                            Se ei ole selvinnyt vain viittä vuosisataa, mutta myös loikan elektroniseen kirjoitukseen, jääden suurinpiirtein muuntamattomana.
+                            Se tuli kuuluisuuteen 1960-luvulla kun Letraset-paperiarkit, joissa oli Lorem Ipsum pätkiä, julkaistiin ja
+                            vielä myöhemmin tietokoneen julkistusohjelmissa, kuten Aldus PageMaker joissa oli versioita Lorem Ipsumista.</p>
+                    </div>
+                    <div className="col">
+                        <p className="content-paragraph">Lorem Ipsum on yksinkertaisesti testausteksti, jota tulostus- ja ladontateollisuudet käyttävät. Lorem Ipsum on ollut teollisuuden normaali
+                            testausteksti jo 1500-luvulta asti, jolloin tuntematon tulostaja otti kaljuunan ja sekoitti sen tehdäkseen esimerkkikirjan.
+                            Se ei ole selvinnyt vain viittä vuosisataa, mutta myös loikan elektroniseen kirjoitukseen, jääden suurinpiirtein muuntamattomana.
+                            Se tuli kuuluisuuteen 1960-luvulla kun Letraset-paperiarkit, joissa oli Lorem Ipsum pätkiä, julkaistiin ja
+                            vielä myöhemmin tietokoneen julkistusohjelmissa, kuten Aldus PageMaker joissa oli versioita Lorem Ipsumista.</p>
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
