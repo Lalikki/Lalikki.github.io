6636dfa6da6f5ee81368aa6478dfac0d84c8eb65
Lalikki <laurijarvinen@hotmail.com>
2021-09-07 16:01:36 +0300

    carousel and portfolio page started

---
+++ src/components/Carousel.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Carousel = () => {
+    return (
+        <div className="carousel">
+            <p>asdasdasd</p>
+        </div>
+    )
+}
+
+export default Carousel;
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Carousel from "./Carousel";
 
 const Portfolio = () => {
     return (
-        <div>
-            <h2>Portfolio</h2>
+        <div className="portfolio-wrapper">
+            <div className="container">
+
+                <h1>Portfolio</h1>
+                <Carousel />
+            </div>
         </div>
     )
 }
